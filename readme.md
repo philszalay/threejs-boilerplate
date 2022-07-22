c578770ae30af9f1b4b63947745ce51e11c14813
philszalay <philszalay@aol.de>
2022-07-22 23:42:29 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Three.js Boilerplate
+
+## Setup
+Download [Node.js 16](https://nodejs.org/en/download/).
+
+``` bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
+
+# Build for production int the dist/ directory and publish the folder's content to the "gh-pages" branch
+npm run deploy
+```
---
+++ src/script.js
@@ -0,0 +1,127 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+export default class ThreeJsDraft {
+  constructor () {
+    /**
+     * Variables
+     */
+    this.canvas = document.querySelector('canvas.webgl')
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    this.devicePixelRatio = window.devicePixelRatio
+
+    /**
+     * Scene
+     */
+    this.scene = new THREE.Scene()
+
+    /**
+     * Camera
+     */
+    this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000)
+    this.camera.position.z = 5
+
+    /**
+     * Renderer
+     */
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas
+    })
+    this.renderer.setSize(this.width, this.height)
+    this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2))
+
+    /**
+     * Controls
+     */
+    this.orbitControls = new OrbitControls(this.camera, this.canvas)
+
+    /**
+     * Resize
+     */
+    window.addEventListener('resize', () => {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      this.camera.aspect = this.width / this.height
+      this.camera.updateProjectionMatrix()
+
+      this.devicePixelRatio = window.devicePixelRatio
+
+      this.renderer.setSize(this.width, this.height)
+      this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2))
+    }, false)
+
+    /**
+     * Loading Manager
+     */
+    this.loadingManager = new THREE.LoadingManager()
+
+    this.loadingManager.onStart = function (url, itemsLoaded, itemsTotal) {
+      console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+    }
+
+    this.loadingManager.onLoad = function () {
+      console.log('Loading complete!')
+    }
+
+    this.loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
+      console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+    }
+
+    this.loadingManager.onError = function (url) {
+      console.log('There was an error loading ' + url)
+    }
+
+    /**
+     * Load Assets
+     */
+    this.loadAssets()
+
+    /**
+     * Helpers
+     */
+    this.addHelpers()
+
+    /**
+     * Objects
+     */
+    this.addObjects()
+
+    this.renderer.render(this.scene, this.camera)
+    /**
+     * Animation Loop
+     */
+    this.animate()
+  }
+
+  loadAssets () {
+    // const textureLoader = new THREE.TextureLoader(this.loadingManager)
+  }
+
+  addHelpers () {
+    const axisHelper = new THREE.AxisHelper(3)
+    this.scene.add(axisHelper)
+  }
+
+  addObjects () {
+    const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
+    const cubeMaterial = new THREE.MeshBasicMaterial({
+      color: 0x00ff00
+    })
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+    this.scene.add(cube)
+  }
+
+  animate () {
+    this.orbitControls.update()
+    this.renderer.render(this.scene, this.camera)
+    window.requestAnimationFrame(this.animate.bind(this))
+  }
+}
+
+/**
+ * Create ThreeJsDraft
+ */
+// eslint-disable-next-line no-new
+new ThreeJsDraft()
